9e47dd395f374cac02b9b59714ec9b2fc3d7c0e4
MokkeMeguru <meguru.mokke@gmail.com>
2020-02-01 13:04:24 +0900

    add dependencies

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import setuptools
 
 setuptools.setup(
     name='TFGENZOO',
+    packages=setuptools.find_packages(),
+    install_requires=['tensorflow>=2.1.0', 'tensorflow_probability>=0.9.0'],
     version='0.1.0alpha',
     author='MokkeMeguru',
     author_email='meguru.mokke@gmail.com',
@@ -10,7 +12,8 @@ setuptools.setup(
                       + 'generative model examples / layers'),
     long_description_content_type='text/markdown',
     url='https://github.com/MokkeMeguru/TFGENZOO',
-    packages=setuptools.find_packages(),
+    
+    licence='MIT',
     classifiers=[
         "Programming Language :: Python :: 3.7.6",
         "Licence :: OSI Apprived :: MIT Licence",

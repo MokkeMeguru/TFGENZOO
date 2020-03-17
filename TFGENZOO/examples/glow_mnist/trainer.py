06d0be70f060415f594ebb1adbdd4c897e56610b
Mokke Meguru <meguru.mokke@gmail.com>
2020-03-17 12:28:41 +0900

    Update trainer.py

--- TFGENZOO/examples/glow_mnist/trainer.py
+++ TFGENZOO/examples/glow_mnist/trainer.py
@@ -39,24 +39,20 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
 
 
 def bits_x(log_likelihood: tf.Tensor,
-           log_det_jacobian: tf.Tensor, pixels: int):
+           log_det_jacobian: tf.Tensor, pixels: int, n_bits: int = 8):
     """bits/dims
-    args:
-    - log_likelihood: tf.Tensor
-    shape is [batch_size,]
-    - log_det_jacobian: tf.Tensors
-    shape is [batch_size,]
-    - pixels: int
-    pixels
-    ex. HWC image => H * W * C
-    returns:
-    - bits_x: tf.Tensor
-    shape is [batch_size,]
+    Args:
+        log_likelihood: shape is [batch_size,]
+        log_det_jacobian: shape is [batch_size,]
+        pixels: e.g. HWC image => H * W * C
+        n_bits: e.g [0 255] image => 8 = log(256)
+    Returns:
+        bits_x: shape is [batch_size,]
     formula:
-    - (log_likelihood + log_det_jacobian) / (log 2 * h * w * c)
+    - (log_likelihood + log_det_jacobian) / (log 2 * h * w * c) + log(2^n_bits) / log(2.)
     """
     nobj = - 1.0 * (log_likelihood + log_det_jacobian)
-    _bits_x = nobj / (np.log(2.) * pixels)
+    _bits_x = nobj / (np.log(2.) * pixels) + n_bits
     return _bits_x
 
 

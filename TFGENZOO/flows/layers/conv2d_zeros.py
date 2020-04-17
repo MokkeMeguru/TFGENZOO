2dd301a3d7dc36921406782d4a66ddf837f41c05
MokkeMeguru <meguru.mokke@gmail.com>
2020-04-17 13:07:59 +0900

    remove caching in actions

--- TFGENZOO/flows/layers/conv2d_zeros.py
+++ TFGENZOO/flows/layers/conv2d_zeros.py
@@ -18,24 +18,32 @@ class Conv2DZeros(Layer):
     def __init__(self, width: int,
                  filter_size: List, stride: List,
                  pad: str = "SAME", logscale_factor: int = 3,
-                 skip=1, edge_bias: bool = True):
+                 skip=1):
         """
+        Args:
+            width          : output hidden size
+            filter_size    : convolution's filter_size e.g. [3, 3]
+            stride         : stride size e.g. [1, 1]
+            pad            : padding method choices ["SAME", "VALID"]
+            logscale_factor: logscale_factor in Glow's proposal
+            skip           : the flag of atrous convolution
         """
         super().__init__()
         assert len(stride) == 2, "not supported stride size {}".format(stride)
         assert len(filter_size) == 2,\
             "not supported filter size {}".format(filter_size)
+        if skip != 1:
+            assert stride == [1, 1], "for atrous convolution, stride shold be [1, 1]"
         self.width = width
         self.filter_size = filter_size
         self.stride = stride
         self.pad = pad
         self.logscale_factor = logscale_factor
         self.skip = skip
-        self.edge_bias = edge_bias
 
 
     def build(self, input_shape: tf.TensorShape):
-        n_in  = input_shape[3]
+        n_in = input_shape[3]
         self.stride_shape = [1] + self.stride + [1]
         self.filter_size = self.filter_size + [n_in, self.width]
         self.W = self.add_weight(

34e4a1f71e1d10b2c2c8a4bdc6c1250a521fee56
MokkeMeguru <meguru.mokke@gmail.com>
2020-04-24 15:54:29 +0900

    create optimizer repo

--- TFGENZOO/flows/utils/util.py
+++ TFGENZOO/flows/utils/util.py
@@ -1,5 +1,6 @@
 from typing import Callable, Dict, Union
 
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model, layers
 
@@ -7,6 +8,27 @@ from TFGENZOO.flows.utils.conv import Conv2D
 from TFGENZOO.flows.utils.conv_zeros import Conv2DZeros
 
 Layer = layers.Layer
+
+
+def bits_x(log_likelihood: tf.Tensor,
+           log_det_jacobian: tf.Tensor, pixels: int, n_bits: int = 8):
+    """bits/dims
+    Args:
+        log_likelihood: shape is [batch_size,]
+        log_det_jacobian: shape is [batch_size,]
+        pixels: e.g. HWC image => H * W * C
+        n_bits: e.g [0 255] image => 8 = log(256)
+
+    Returns:
+        bits_x: shape is [batch_size,]
+
+    formula:
+        (log_likelihood + log_det_jacobian)
+          / (log 2 * h * w * c) + log(2^n_bits) / log(2.)
+    """
+    nobj = - 1.0 * (log_likelihood + log_det_jacobian)
+    _bits_x = nobj / (np.log(2.) * pixels) + n_bits
+    return _bits_x
 
 
 def split_feature(x: tf.Tensor, type: str = "split"):

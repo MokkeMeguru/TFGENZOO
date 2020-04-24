775042d46feaebc01eaab1414b6a974cf190a9ff
MokkeMeguru <meguru.mokke@gmail.com>
2020-04-24 17:56:33 +0900

    remove affine_coupling layer's safety

--- TFGENZOO/flows/affine_coupling.py
+++ TFGENZOO/flows/affine_coupling.py
@@ -130,7 +130,7 @@ class AffineCoupling(FlowComponent):
             shift = h[..., 0::2]
             log_scale = h[..., 1::2]
 
-            scale = tf.nn.sigmoid(log_scale + 2.0) + 1e-10
+            scale = tf.nn.sigmoid(log_scale + 2.0)
             # scale = tf.exp(tf.clip_by_value(log_scale, -15.0, 15.0))
             z2 = (x2 + shift) * scale
 

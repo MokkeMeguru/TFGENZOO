2980e245085c1b078b17a4878d2678089a9771f8
Mokke Meguru <meguru.mokke@gmail.com>
2020-05-18 15:59:29 +0900

    aggregation for multi-gpu/tpu

--- TFGENZOO/flows/flowbase.py
+++ TFGENZOO/flows/flowbase.py
@@ -32,7 +32,9 @@ class FlowBase(Layer, metaclass=ABCMeta):
 
     def build(self, input_shape: tf.TensorShape):
         self.initialized = self.add_weight(
-            name="initialized", dtype=tf.bool, trainable=False
+            name="initialized", dtype=tf.bool, 
+            trainable=False,
+            aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA
         )
         self.initialized.assign(False)
         self.built = True

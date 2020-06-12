6b64d382af39d6d277344af4ed4da7cc1448a8a1
MokkeMeguru <meguru.mokke@gmail.com>
2020-06-12 11:56:20 +0900

    fix affine_coupling's test

--- TFGENZOO/flows/affine_coupling_test.py
+++ TFGENZOO/flows/affine_coupling_test.py
@@ -9,7 +9,9 @@ from TFGENZOO.layers.resnet import ShallowResNet
 class AffineCouplingTest(tf.test.TestCase):
     def setUp(self):
         super(AffineCouplingTest, self).setUp()
-        self.affine_coupling = AffineCoupling(scale_shift_net=ShallowResNet(width=128))
+        self.affine_coupling = AffineCoupling(
+            scale_shift_net=lambda inputs: ShallowResNet(inputs, width=128)
+        )
         self.affine_coupling.build((None, 16, 16, 4))
 
     def testAffineCouplingInitialize(self):
@@ -29,7 +31,6 @@ class AffineCouplingTest(tf.test.TestCase):
         self.assertShapeEqual(np.zeros([1024]), ldj)
         self.assertAllClose(x, rev_x, rtol=3000, atol=1)
         self.assertAllClose(ldj + ildj, tf.zeros([1024]), rtol=1e-1, atol=1e-1)
-
 
 
 if __name__ == "__main__":

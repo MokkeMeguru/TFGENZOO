84185b56e167e0d73fe779d0326e901945c4c5d0
MokkeMeguru <meguru.mokke@gmail.com>
2020-07-16 21:53:24 +0900

    fix typo

--- TFGENZOO/flows/squeeze.py
+++ TFGENZOO/flows/squeeze.py
@@ -6,7 +6,7 @@ from TFGENZOO.flows.flowbase import FlowBase
 class SqueezeBase(FlowBase):
     def __init__(self, n_squeeze: int = 2, **kwargs):
         super().__init__(**kwargs)
-        self.n_queeze = n_squeeze
+        self.n_squeeze = n_squeeze
 
     def get_config(self):
         config = super().get_config()
